836d169356e2f4252803d4d7f27bebf0b92feb20
aangairbender <jetixranger@ya.ru>
2021-05-14 22:19:06 +0900

    initial commit

---
+++ CGSingleFiler/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CGSingleFiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("First argument should be a path to project root (containing .csproj)");
+                Console.WriteLine("Second argument should be a path to merged file");
+                return;
+            }
+            var dir = new DirectoryInfo(args[0]);
+            if (!dir.Exists)
+            {
+                Console.WriteLine($"Directory {dir.FullName} does not exist");
+                return;
+            }
+
+            if (dir.GetFiles().Any(f => f.Extension == ".csproj") == false)
+            {
+                Console.WriteLine($"Directory {dir.FullName} does not contain .csproj file");
+                return;
+            }
+
+            var output = new FileInfo(args[1]);
+            if (!output.Directory.Exists)
+            {
+                Console.WriteLine($"Directory {output.Directory.FullName} does not exist");
+                return;
+            }
+
+            Job(dir, output);
+        }
+
+        private static void Job(DirectoryInfo projectDir, FileInfo outputFile)
+        {
+            var sourceFiles = new List<SourceFile>();
+            ProcessDir(projectDir, sourceFiles);
+
+            var usingLines = sourceFiles.SelectMany(s => s.Usings).Distinct();
+            var namespaceLines = sourceFiles.SelectMany(s => new List<string> { "\n" }.Concat(s.NamespaceLines));
+
+            File.WriteAllLines(outputFile.FullName, usingLines.Concat(namespaceLines).ToArray());
+        }
+
+        private static void ProcessDir(DirectoryInfo dir, List<SourceFile> sourceFiles)
+        {
+            foreach (var subdir in dir.EnumerateDirectories())
+                ProcessDir(subdir, sourceFiles);
+
+            foreach (var file in dir.EnumerateFiles())
+            {
+                if (file.Extension != ".cs")
+                    continue;
+                sourceFiles.Add(new SourceFile(file));
+            }
+        }
+    }
+}
---
+++ CGSingleFiler/SourceFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CGSingleFiler
+{
+    class SourceFile
+    {
+        public List<string> Usings { get; }
+        public List<string> NamespaceLines { get; }
+        public SourceFile(FileInfo fileInfo)
+        {
+            Usings = new List<string>();
+            NamespaceLines = new List<string>();
+
+            var lines = File.ReadAllLines(fileInfo.FullName);
+            var namespaceStarted = false;
+
+            foreach (var line in lines)
+            {
+                if (namespaceStarted)
+                {
+                    NamespaceLines.Add(line);
+                    continue;
+                }
+
+                if (line == string.Empty)
+                    continue;
+
+                if (line.StartsWith("using"))
+                {
+                    Usings.Add(line);
+                } else if (line.StartsWith("namespace"))
+                {
+                    namespaceStarted = true;
+                    NamespaceLines.Add(line);
+                }
+            }
+        }
+    }
+}
